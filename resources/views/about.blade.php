f9400d37373734f6398824478f86a46ea722116d
tiagoroccatagliata <tiagoroccatagliata@gail.com>
2025-01-01 10:44:51 -0300

    first commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,2 @@
+<h1>Hello, Welcome to my Laravel Project.</h1>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad ea ex ipsam minima nam necessitatibus obcaecati! Aperiam, eos, ex? Commodi eveniet fugiat illum incidunt inventore molestias non officia sapiente vel.</p>
