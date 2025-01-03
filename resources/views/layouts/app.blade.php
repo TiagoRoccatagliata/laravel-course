6560f49858e60d0f38fac6f6e7443be8ffbdadbb
tiagoroccatagliata <tiagoroccatagliata@gail.com>
2025-01-03 10:30:28 -0300

    Layouts Update

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.clean')
+
+@section('childContent')
+    @include('layouts.partials.header')
+    @yield('content')
+    @hasSection('footerLinks')
+        <footer>
+            @yield('footerLinks')
+        </footer>
+    @endif
+@endsection
+
--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,13 @@
 <?php
 
 
-use App\Http\Controllers\HelloController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\SignupController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index'])
     ->name('home');
 
-Route::view('/about', 'about' )->name('about');
-
-Route::get('/welcome', [HelloController::class, 'welcome'])
-    ->name('welcome');
+Route::get('/signup', [SignupController::class, 'create']);
+Route::get('/login', [LoginController::class, 'create']);
